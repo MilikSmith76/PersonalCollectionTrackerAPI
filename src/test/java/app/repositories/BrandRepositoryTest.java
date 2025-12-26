8170d0f0953d4dca648e451e057eb2f3aeb9f377
MilikSmith76 <miliksmith23@gmail.com>
2025-12-26 16:02:28 -0500

    Update Brand Repository unit tests

--- src/test/java/app/repositories/BrandRepositoryTest.java
+++ src/test/java/app/repositories/BrandRepositoryTest.java
@@ -3,8 +3,11 @@ package app.repositories;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import app.entities.BrandDAO;
+import app.generated.types.BrandFilter;
 import app.test.utils.EntityDaoCreator;
 import app.test.utils.SqlFiles;
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -45,7 +48,7 @@ public class BrandRepositoryTest {
     }
 
     @Test
-    public void shouldFindByIdThenReturnBrand() {
+    public void findByIdShouldFindBrand1() {
         // Arrange
 
         // Act
@@ -56,5 +59,118 @@ public class BrandRepositoryTest {
         assertThat(result.get().getId()).isEqualTo(brand1.getId());
         assertThat(result.get().getName()).isEqualTo(brand1.getName());
         assertThat(result.get().getLogoUrl()).isEqualTo(brand1.getLogoUrl());
+    }
+
+    @Test
+    public void findByIdShouldFindBrand2() {
+        // Arrange
+
+        // Act
+        Optional<BrandDAO> result = brandRepository.findById(brand2.getId());
+
+        // Assert
+        assertThat(result.isPresent()).isEqualTo(true);
+        assertThat(result.get().getId()).isEqualTo(brand2.getId());
+        assertThat(result.get().getName()).isEqualTo(brand2.getName());
+        assertThat(result.get().getLogoUrl()).isEqualTo(brand2.getLogoUrl());
+    }
+
+    @Test
+    public void findByIdShouldFindNothingForNonExistentId() {
+        // Arrange
+
+        // Act
+        Optional<BrandDAO> result = brandRepository.findById(0L);
+
+        // Assert
+        assertThat(result.isPresent()).isEqualTo(false);
+    }
+
+    @Test
+    public void findByCriteriaShouldFilterCorrectly() {
+        // Arrange
+        BrandFilter filter = BrandFilter.newBuilder().name("Brand").build();
+
+        // Act
+        List<BrandDAO> result = brandRepository.findByCriteria(filter);
+
+        // Assert
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.getFirst().getId()).isEqualTo(brand1.getId());
+        assertThat(result.getFirst().getName()).isEqualTo(brand1.getName());
+        assertThat(result.getFirst().getLogoUrl())
+            .isEqualTo(brand1.getLogoUrl());
+        assertThat(result.getLast().getId()).isEqualTo(brand2.getId());
+        assertThat(result.getLast().getName()).isEqualTo(brand2.getName());
+        assertThat(result.getLast().getLogoUrl())
+            .isEqualTo(brand2.getLogoUrl());
+    }
+
+    @Test
+    public void findByCriteriaShouldDoComplexFilterCorrectly() {
+        // Arrange
+        String description = "This is a test description";
+        brand2.setDescription(description);
+        BrandFilter filter = BrandFilter
+            .newBuilder()
+            .name("Brand 2")
+            .description(description)
+            .build();
+
+        // Act
+        List<BrandDAO> result = brandRepository.findByCriteria(filter);
+
+        // Assert
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.getFirst().getId()).isEqualTo(brand2.getId());
+        assertThat(result.getFirst().getName()).isEqualTo(brand2.getName());
+        assertThat(result.getFirst().getLogoUrl())
+            .isEqualTo(brand2.getLogoUrl());
+        assertThat(result.getFirst().getDescription())
+            .isEqualTo(brand2.getDescription());
+    }
+
+    @Test
+    public void findByCriteriaShouldFindNothingForFilterWithNoMatches() {
+        // Arrange
+        BrandFilter filter = BrandFilter.newBuilder().name("Brand 3").build();
+
+        // Act
+        List<BrandDAO> result = brandRepository.findByCriteria(filter);
+
+        // Assert
+        assertThat(result.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void saveShouldCreateNewBrand() {
+        // Arrange
+        BrandDAO brand3 = EntityDaoCreator.createBrand(
+            "Brand 3",
+            "brand_3_logo_url"
+        );
+
+        // Act
+        BrandDAO result = brandRepository.save(brand3);
+
+        // Assert
+        assertThat(result.getName()).isEqualTo(brand3.getName());
+        assertThat(result.getLogoUrl()).isEqualTo(brand3.getLogoUrl());
+    }
+
+    @Test
+    public void saveShouldUpdateBrand() {
+        // Arrange
+        brand1.setName("Brand 1 Updated");
+        brand1.setDeleted(true);
+        brand1.setDeletedAt(new Timestamp(System.currentTimeMillis()));
+
+        // Act
+        BrandDAO result = brandRepository.save(brand1);
+
+        // Assert
+        assertThat(result.getName()).isEqualTo(brand1.getName());
+        assertThat(result.getDeleted()).isEqualTo(brand1.getDeleted());
+        assertThat(result.getDeletedAt()).isEqualTo(brand1.getDeletedAt());
     }
 }
